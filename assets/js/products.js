a0dccd894b26868e273ad455b5b1549a1f66ffc4
Ваше Имя на GitHub <_@example.com>
2026-04-13 03:11:25 +0300

    Fix UI styles, catalog display issue

---
+++ assets/js/products.js
@@ -0,0 +1,10 @@
+const products = [
+    {
+        "title": "Классическая свеча",
+        "price": 1500,
+        "description": "Изысканная классическая свеча ручной работы.",
+        "has_scent": true,
+        "has_color": true,
+        "image": "covers/cover_classic.png"
+    }
+];
